e29e77266b927ffad7606993c2cf73bdfac54db7
siyukatu <120154870+siyukatu@users.noreply.github.com>
2024-11-26 18:56:05 +0900

    Clean-CSSがネストCSSに対応していないため削除

--- build.js
+++ build.js
@@ -1,16 +1,15 @@
 const UglifyJS = require("uglify-js");
-const CleanCSS = require("clean-css");
 const fs = require("fs");
 
 var main_css = fs.readFileSync("main.css", "utf8");
 
-var minified_css = new CleanCSS().minify(main_css).styles;
+var minified_css = main_css.replace(/\r?\n/g, "").replace(/\s+/g, " ").replace(/\/\*.*?\*\//g, "").replace(/;}/g, "}").replace(/} /g, "}").replace(/ {/g, "{").replace(/{ /g, "{").replace(/, /g, ",").replace(/: /g, ":").replace(/; /g, ";").replace(/, /g, ",");
 
-var main_js = fs.readFileSync("main.js", "utf8").replace("`[Minified CSS Data]`", "`"+minified_css.replace(/\\/g, "\\\\").replace(/`/g, "\\`")+"`");
+var main_js = fs.readFileSync("main.js", "utf8");
 var minified = UglifyJS.minify(main_js);
 
 if (!fs.existsSync("output")){
     fs.mkdirSync("output");
 }
-console.log(minified);
-fs.writeFileSync("output/main.js", minified.code);
+console.log(minified_css);
+fs.writeFileSync("output/main.js", minified.code.replace('"[[[Minified CSS Data]]]"', "`"+minified_css.replace(/\\/g, "\\\\").replace(/`/g, "\\`")+"`"));
